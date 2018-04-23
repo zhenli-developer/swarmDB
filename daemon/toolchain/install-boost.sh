3e93c432b73075a5f31a6e47bd768871ee464556
amastracci <andrew@bluzelle.com>
2018-04-23 12:06:19 -0700

    Fix tar command for linux

--- daemon/toolchain/install-boost.sh
+++ daemon/toolchain/install-boost.sh
@@ -19,13 +19,13 @@ fi
 BOOST_LIBS="test,chrono,coroutine,program_options,random,regex,system,thread"
 BOOST_VERSION_UNDERSCORES="$(echo "${BOOST_VERSION}" | sed 's/\./_/g')"
 BOOST_TARBALL="boost_${BOOST_VERSION_UNDERSCORES}.tar.gz"
-BOOST_URL="http://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION}/${BOOST_TARBALL}/download"
+BOOST_URL="http://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION}/${BOOST_TARBALL}"
 BOOST_PACKAGE_DIR=${BOOST_INSTALL_DIR}/${BOOST_VERSION_UNDERSCORES}
 
 if [ ! -e ${BOOST_PACKAGE_DIR}/libs ]; then
-    echo "Boost not found in the cache, get and extract it..."
+    echo "Boost not found in the cache, get and extract it from ${BOOST_URL}"
     mkdir -p ${BOOST_PACKAGE_DIR}
-    curl -L ${BOOST_URL} | tar -xj -C ${BOOST_PACKAGE_DIR} --strip-components=1
+    curl -L ${BOOST_URL} | tar -xz -C ${BOOST_PACKAGE_DIR} --strip-components=1
     cd ${BOOST_PACKAGE_DIR}
     ./bootstrap.sh --prefix=${BOOST_PACKAGE_DIR} --with-libraries=${BOOST_LIBS}
     ./b2 install -d0
